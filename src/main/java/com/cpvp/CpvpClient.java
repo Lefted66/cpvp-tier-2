f386504ad75a542e746742bdb956cd42b36cd73d
Lefted66 <129308499+Lefted66@users.noreply.github.com>
2026-03-28 08:10:57 +0000

    CpvpClient.java

---
+++ src/main/java/com/cpvp/CpvpClient.java
@@ -0,0 +1,36 @@
+package com.cpvp;
+
+import com.cpvp.modules.AutoTotemModule;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CpvpClient implements ClientModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("cpvptier2");
+    public static final AutoTotemModule AUTO_TOTEM = new AutoTotemModule();
+    public static KeyBinding toggleKey;
+
+    @Override
+    public void onInitializeClient() {
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.cpvptier2.autototem",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_H,
+                "category.cpvptier2"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (toggleKey.wasPressed()) AUTO_TOTEM.toggle();
+            if (client.player == null || client.world == null) return;
+            AUTO_TOTEM.onTick(client);
+        });
+
+        LOGGER.info("[cPvP Tier 2] Loaded.");
+    }
+}
